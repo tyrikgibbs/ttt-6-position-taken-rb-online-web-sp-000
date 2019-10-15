7adb44f1553da9d6791edca93e2afb935a590924
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-15 17:44:58 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,7 +8,7 @@ end
 
 # code your #position_taken? method here!
 
-def position_taken? (board, index)
+def position_taken?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil
     return false
   else
