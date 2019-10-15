dc50338154ce3b4822d46bef990d9e662ac98de8
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-10-15 17:36:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,12 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 # code your #position_taken? method here!
-
-
 
 def position_taken? (board, index)
   if (index == "" or " ")
